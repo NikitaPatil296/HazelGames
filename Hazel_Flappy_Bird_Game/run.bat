fb8e7b0a5ac5721bf3e1b5e5b999102732225786
Rohan More <rohanpmore1998@gmail.com>
2025-04-15 14:47:01 +0200

    Hazel flappy bird game

---
+++ Hazel_Flappy_Bird_Game/run.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Navigate to the Sandbox folder to ensure relative asset paths work
+cd /d N:\Hazel_Flappy_Bird_Game\Sandbox
+
+REM Run the executable from the bin output path
+..\Sandbox\Sandbox.exe
+
+pause
